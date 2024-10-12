bce444ecca35b8e1adedcffc96db8af8c66b08b8
Heike13 <matthias.hibon@sfr.fr>
2024-10-12 19:51:44 +0200

    migration with doctrine ORM

---
+++ src/Entity/Task.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\TaskRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+#[ApiResource]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: false)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: false)]
+    private ?string $content = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: false)]
+    private ?\DateTimeInterface $dueDate = null;
+
+    #[ORM\Column(type: Types::BOOLEAN, nullable: false)]
+    private ?bool $isComplete = false;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $assignedTo = null;
+
+    // Getters and setters
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getDueDate(): ?\DateTimeInterface
+    {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(\DateTimeInterface $dueDate): self
+    {
+        $this->dueDate = $dueDate;
+
+        return $this;
+    }
+
+    public function getIsComplete(): ?bool
+    {
+        return $this->isComplete;
+    }
+
+    public function setIsComplete(bool $isComplete): self
+    {
+        $this->isComplete = $isComplete;
+
+        return $this;
+    }
+
+    public function getAssignedTo(): ?string
+    {
+        return $this->assignedTo;
+    }
+
+    public function setAssignedTo(?string $assignedTo): self
+    {
+        $this->assignedTo = $assignedTo;
+
+        return $this;
+    }
+}
